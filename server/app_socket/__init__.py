3986d8df4bba425a2bb5676a8b057b2d31bcb6a2
Kristi Hwang <kristi.y.l.h@gmail.com>
2025-08-04 14:33:43 -0700

    set up backend with models and routes

---
+++ server/app_socket/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_socketio import SocketIO
+from .models import db
+from .routes import api
+from .config import Config
+
+socketio = SocketIO(cors_allowed_origins="*")
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    app.register_blueprint(api, url_prefix="/api")
+    socketio.init_app(app)
+
+    return app
