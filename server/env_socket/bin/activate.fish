ea909bb878039bdefa949f93dc00ca952e689fff
Ryan <ryan@anderson66@protonmail.com>
2025-08-07 12:05:59 -0400

    updated readme

--- server/env_socket/bin/activate.fish
+++ server/env_socket/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/kristihwang/Desktop/V56-tier3-team-39/server/env_socket"
+set -gx VIRTUAL_ENV "/home/ryananderson66/V56-tier3-team-39/server/env_socket"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
